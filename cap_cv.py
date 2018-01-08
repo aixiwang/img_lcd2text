ac47180e73d3c441e32f26e0a9e3fbb305a51e6d
Aixi Wang <aixi.wang@hotmail.com>
2018-01-08 15:25:03 +0800

    Update cap_cv.py

--- cap_cv.py
+++ cap_cv.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python
 #
-# python-v4l2capture
+# Copyright(c) 2017 Aixi Wang (aixi.wang@hotmail.com)
 #
-# This file is an example on how to capture a picture with
-# python-v4l2capture.
-#
-# 2009, 2010 Fredrik Portstrom
-#
-# I, the copyright holder of this file, hereby release it into the
-# public domain. This applies worldwide. In case this is not legally
-# possible: I grant anyone the right to use this work for any
-# purpose, without any conditions, unless such conditions are
-# required by law.
+#==========================================================
+
 
 #from PIL import Image
 #import select
